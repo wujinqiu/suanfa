cd96a32d7b12a0ba1d245733b4bd70ad57d16a23
wujinqiu <wujinqiu2013@163.com>
2020-06-01 18:27:27 +0800

    6_1

--- 6_1.js
+++ 6_1.js
@@ -15,3 +15,25 @@ var integerBreak = function (n) {
 }
 
 integerBreak(7)
+
+// 给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
+
+// 对每一个孩子，检查是否存在一种方案，将额外的 extraCandies 个糖果分配给孩子们之后，此孩子有 最多 的糖果。注意，允许有多个孩子同时拥有 最多 的糖果数目。
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  let max_num = Math.max(...candies)
+  let arr = new Array(candies.length)
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max_num) {
+      arr[i] = true
+    } else {
+      arr[i] = false
+    }
+  }
+  return arr
+}
